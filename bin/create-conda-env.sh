2b844cff62d68380ba0832195548177929939c1f
pughdr <david.pugh@kaust.edu.sa>
2020-03-24 19:56:51 +0300

    Working build of horovod for tf 2.1,pytorch and mxnet

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -1,13 +1,4 @@
 #!/bin/bash --login
-#SBATCH --time=30:00
-#SBATCH --mem=24G
-#SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:1
-#SBATCH --partition=debug
-#SBATCH --job-name=create-conda-env
-#SBATCH --mail-type=ALL
-#SBATCH --output=bin/%x-%j-slurm.out
-#SBATCH --error=bin/%x-%j-slurm.err
 
 # set relevant build variables for horovod
 export ENV_PREFIX=$PWD/env
