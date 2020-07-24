931012e5f34fd2e360b83ad26b4ddab0f20b217d
pughdr <david.pugh@kaust.edu.sa>
2020-07-24 09:43:54 +0300

    Refactored build scripts

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -4,8 +4,7 @@
 export ENV_PREFIX=$PWD/env
 export HOROVOD_CUDA_HOME=$CUDA_HOME
 export HOROVOD_NCCL_HOME=$ENV_PREFIX
-export HOROVOD_GPU_ALLREDUCE=NCCL
-export HOROVOD_GPU_BROADCAST=NCCL
+export HOROVOD_GPU_OPERATIONS=NCCL
 
 # create the conda environment
 conda env create --prefix $ENV_PREFIX --file environment.yml --force
