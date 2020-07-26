6db0c50e9116a53d9d287597477cce5aedae05e5
pughdr <david.pugh@kaust.edu.sa>
2020-07-26 12:07:49 +0300

    Updated config files

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -1,12 +1,21 @@
 #!/bin/bash --login
 
+set -e
+
 # set relevant build variables for horovod
 export ENV_PREFIX=$PWD/env
+export NCCL_HOME=$ENV_PREFIX
 export HOROVOD_CUDA_HOME=$CUDA_HOME
-export HOROVOD_NCCL_HOME=$ENV_PREFIX
-export HOROVOD_GPU_OPERATIONS=NCCL
+export HOROVOD_NCCL_HOME=$NCCL_HOME
+export HOROVOD_GPU_ALLREDUCE=NCCL
+export HOROVOD_GPU_BROADCAST=NCCL
+
+# request builds for specific framework
+export HOROVOD_WITH_TENSORFLOW=1
+export HOROVOD_WITH_PYTORCH=1
+export HOROVOD_WITH_MXNET=1
 
 # create the conda environment
 conda env create --prefix $ENV_PREFIX --file environment.yml --force
 conda activate $ENV_PREFIX
-. postBuild
+source postBuild
